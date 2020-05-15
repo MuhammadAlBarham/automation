e6d34a7d1771fdb9f40c9c3b31144e4fda9ce033
Muhammad Al-Barham <mohammedbrham98@gmail.com>
2020-05-15 21:33:08 +0300

    Adding C++ demonstration file to for loop with Visual Studio Code

---
+++ For loop and If statement/C++_test_Code/test.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+
+int main () {
+
+
+    int A = 0 ; 
+    for(int i=0; i<10; ++i)
+    {
+     A = A + 5 ; 
+    }
+
+    std::cout << A << std::endl ; 
+    
+    return 0 ; 
+}
